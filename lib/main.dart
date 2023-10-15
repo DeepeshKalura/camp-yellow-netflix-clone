a22741f48cdced698bdf448b67498621bcc83aeb
Deepesh Kalura <deepeshkalurs@gmail.com>
2023-10-15 16:57:33 +0530

    added theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import 'bloc/navigation/routes.dart';
 import 'bloc/navigation/routes_name.dart';
@@ -23,6 +24,64 @@ class MyApp extends StatelessWidget {
           elevation: 0,
           iconTheme: IconThemeData(
             color: Colors.white,
+          ),
+        ),
+        textTheme: TextTheme(
+          titleSmall: GoogleFonts.montserrat(
+            color: Colors.white,
+            fontSize: 24,
+            fontWeight: FontWeight.w700,
+            height: 0,
+          ),
+          bodyMedium: GoogleFonts.libreFranklin(
+            color: Colors.white,
+            fontSize: 16,
+            fontWeight: FontWeight.w700,
+            height: 0,
+          ),
+          bodySmall: GoogleFonts.libreFranklin(
+            color: Colors.white,
+            fontSize: 12,
+            fontWeight: FontWeight.w700,
+            height: 0,
+          ),
+          labelSmall: GoogleFonts.ebGaramond(
+            color: const Color(0xFFB3B3B3),
+            fontSize: 12,
+            fontWeight: FontWeight.w800,
+            height: 0,
+          ),
+        ),
+        textButtonTheme: TextButtonThemeData(
+          style: ButtonStyle(
+            iconColor: MaterialStateProperty.all(Colors.white),
+            textStyle: MaterialStateProperty.all(
+              GoogleFonts.libreFranklin(
+                color: Colors.white,
+                fontSize: 16,
+                fontWeight: FontWeight.w700,
+                height: 0,
+              ),
+            ),
+          ),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.all(
+              const Color(0xFF262626),
+            ),
+            textStyle: MaterialStateProperty.all(
+              Theme.of(context).textTheme.bodyMedium,
+            ),
+            iconColor: const MaterialStatePropertyAll(Colors.white),
+            shape: MaterialStateProperty.all(
+              RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(5),
+              ),
+            ),
+            fixedSize: MaterialStateProperty.all(
+              const Size(double.infinity, 50),
+            ),
           ),
         ),
       ),
